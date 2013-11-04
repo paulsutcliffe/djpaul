06c54f7b4e05aac1a0654c9c1df7af28887de2da
Paul Sutcliffe <paul@kosmyka.com>
2013-11-04 15:55:35 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Djpaul::Application.config.secret_token = '0655865cfb5b27f19b06c74996cc483b4e4f33f3395d9bfaf82725a16d555df0e99a8f57d4e20039446ec4a9f88a1cd7a09fbdd2a2725236f64cbaa82e781002'
