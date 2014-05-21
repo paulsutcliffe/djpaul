aa8f613c0fb66de9c6699bda0822a210e71d6f0e
Paul Sutcliffe <paul@kosmyka.com>
2014-05-21 14:59:45 -0500

    agente

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -25,11 +25,11 @@ server 'kosmyka.com', user: 'paul', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  keys: %w(/home/paulsutcliffe/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(password)
+}
 #
 # And/or per server (overrides global)
 # ------------------------------------
