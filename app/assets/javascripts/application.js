374d2bbba3c3669d25ce054fcaa2db99e889d1e9
Paul Sutcliffe <paul@kosmyka.com>
2014-05-21 22:30:35 -0500

    unicron

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-// require bootstrap
+// require_tree .
+//= require bootstrap
 //= require config
+//
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :repo_url, 'git@github.com:paulsutcliffe/djpaul.git'
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -38,6 +38,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
+    invoke unicorn:restart
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
